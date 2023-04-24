f7a550f60ba8066cfb3985d833b769b9468fda71
lucy-mureithi <lucymureithi0@gmail.com>
2023-04-24 14:29:36 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- *
- * main - Entry 
+ * main - Entry point
  *
  * Return: Always 0
  */
@@ -10,4 +9,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-	
