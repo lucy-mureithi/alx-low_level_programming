031da3caacd848001416a6f2261efebb829c3c51
lucy-mureithi <lucymureithi0@gmail.com>
2023-04-26 22:14:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,18 @@
  * main - Entry point
  * Description: 'prints -putchar to stdout'
  * Return: always 0
- */
+*/
+
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
+
 	{
 	_putchar(c);
 	}
+
 	_putchar('\n');
 }
 
