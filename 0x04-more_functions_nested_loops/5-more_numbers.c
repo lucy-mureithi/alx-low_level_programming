d503cf025f828e151832e713338d8ec9a3ad7e95
lucy-mureithi <lucymureithi0@gmail.com>
2023-04-28 18:02:07 -0700

    alx-low_level_programming# cd 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@
 void more_numbers(void)
 {
 int x, y;
-	for (x = 0; x <= 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 	for (y = 0; y <=  14; y++)
 	{
 	if (y >= 10)
 	_putchar('1');
-	_putchar((y % 10) + '0');
+	_putchar(y % 10 + '0');
 	}
 	_putchar('\n');
 	}
