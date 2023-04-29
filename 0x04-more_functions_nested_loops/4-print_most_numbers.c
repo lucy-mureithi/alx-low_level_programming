10fb2cb2c18089e17e532325db485b970949512d
lucy-mureithi <lucymureithi0@gmail.com>
2023-04-28 17:12:16 -0700

    c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,22 @@
 /**
  * print_most_numbers - Print 0 to 9
  * Description: Exclude 2 and 4
- * Return: numbers 0 to 9
+ * Return: Result
  */
 
 void print_most_numbers(void)
 {
 int x;
-
 for (x = 0; x <= 9; x++)
 {
-	if (!(x == 2 || x == 4))
-		_putchar(x);
+if (x == 0 || x == 4)
+{
+continue;
 }
-_putchar('\n');
+else
+{
+_putchar(x + '0');
+}
+}
+_putchar ('\n');
 }
