050ffb2e3ebd2fb045d6a00ba4e7b5cd306597eb
lucy-mureithi <lucymureithi0@gmail.com>
2023-05-02 01:15:15 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string
+ * @str: The string to print
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+while (; *str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
+
