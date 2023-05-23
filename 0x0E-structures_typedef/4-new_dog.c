6292d38b8b081e89b63bb416d72da6e9d62c132a
lucy-mureithi <lucymureithi0@gmail.com>
2023-05-23 04:01:48 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _stren - return the length of string
+ * @s: string to evaluate
+ * Return: length
+ */
+
+int _stren(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
+
+/**
+ * _strcpy - copy pointed string to src
+ * include terminating null byte
+ * @dest: pointer to copy string
+ * @src: pointer copied
+ * Return: dest
+ */
+
+char *_strcpy(char  *dest, char *src)
+{
+int len, i;
+
+}
